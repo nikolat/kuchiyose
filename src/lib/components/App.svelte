b72f76dab610c09e7f5decbefb7630417e87f954
Don <496156+nikolat@users.noreply.github.com>
2025-05-10 22:40:30 +0900

    表示の調整

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -322,95 +322,103 @@
 				/>
 			</dt>
 			<dd>
-				<dl class="entry">
-					{#each webbookmarks as webbookmark (webbookmark.pubkey)}
-						{@const identifier =
-							webbookmark.tags.find((tag) => tag.length >= 2 && tag[0] === 'd')?.at(1) ?? ''}
-						{@const title =
-							webbookmark.tags.find((tag) => tag.length >= 2 && tag[0] === 'title')?.at(1) ?? ''}
-						{@const hashtags = new Set<string>(
-							webbookmark.tags
-								.filter((tag) => tag.length >= 2 && tag[0] === 't')
-								.map((tag) => tag[1].toLowerCase())
-						)}
-						{@const prof = profileMap.get(webbookmark.pubkey)}
-						{@const naddr = nip19.naddrEncode({
-							kind: webbookmark.kind,
-							pubkey: webbookmark.pubkey,
-							identifier,
-							relays: rc?.getSeenOn(webbookmark.id, true)
-						})}
-						<dt>
-							{#if loginPubkey !== undefined}
-								<button
-									type="button"
-									class="fork"
-									onclick={() => {
-										editDTag = identifier;
-										editTitleTag = title;
-										editTag = '';
-										editTags = Array.from(hashtags);
-										editContent = webbookmark.content;
-										isOpenEdit = true;
-										setTimeout(() => {
-											editTagInput?.focus();
-										}, 10);
-									}}>Fork</button
-								>
-							{/if}
-							<a href="/{naddr}">
-								<time>{new Date(1000 * webbookmark.created_at).toLocaleString()}</time>
-							</a>
-							{#if loginPubkey === webbookmark.pubkey}
-								<span class="bookmark-delete">
-									<button
-										class="bookmark-delete"
-										title="delete the bookmark"
-										onclick={async () => {
-											await sendDeletion(webbookmark);
-										}}
-										aria-label="delete the bookmark"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											width="24"
-											height="24"
-											viewBox="0 0 24 24"
-										>
-											<path
-												fill-rule="evenodd"
-												d="M7,4 L7,3 C7,1.8954305 7.8954305,1 9,1 L15,1 C16.1045695,1 17,1.8954305 17,3 L17,4 L20,4 C21.1045695,4 22,4.8954305 22,6 L22,8 C22,9.1045695 21.1045695,10 20,10 L19.9198662,10 L19,21 C19,22.1045695 18.1045695,23 17,23 L7,23 C5.8954305,23 5,22.1045695 5.00345424,21.0830455 L4.07986712,10 L4,10 C2.8954305,10 2,9.1045695 2,8 L2,6 C2,4.8954305 2.8954305,4 4,4 L7,4 Z M7,6 L4,6 L4,8 L20,8 L20,6 L17,6 L7,6 Z M6.08648886,10 L7,21 L17,21 L17.0034542,20.9169545 L17.9132005,10 L6.08648886,10 Z M15,4 L15,3 L9,3 L9,4 L15,4 Z"
-											/>
-										</svg>
-									</button>
-								</span>
-							{/if}
-							<AddStar
-								sendReaction={getSendReaction(webbookmark)}
-								{sendDeletion}
-								{loginPubkey}
-								{profileMap}
-								eventsReactionToTheTarget={getEventsReactionToTheTarget(
-									webbookmark,
-									eventsReaction
-								)}
-								{eventsEmojiSet}
-								mutedWords={[]}
-							/>
-							<br />
+				{#each webbookmarks as webbookmark (webbookmark.pubkey)}
+					{@const identifier =
+						webbookmark.tags.find((tag) => tag.length >= 2 && tag[0] === 'd')?.at(1) ?? ''}
+					{@const title =
+						webbookmark.tags.find((tag) => tag.length >= 2 && tag[0] === 'title')?.at(1) ?? ''}
+					{@const hashtags = new Set<string>(
+						webbookmark.tags
+							.filter((tag) => tag.length >= 2 && tag[0] === 't')
+							.map((tag) => tag[1].toLowerCase())
+					)}
+					{@const prof = profileMap.get(webbookmark.pubkey)}
+					{@const naddr = nip19.naddrEncode({
+						kind: webbookmark.kind,
+						pubkey: webbookmark.pubkey,
+						identifier,
+						relays: rc?.getSeenOn(webbookmark.id, true)
+					})}
+					<div class="entry">
+						<div class="avatar">
 							<a href="/{nip19.npubEncode(webbookmark.pubkey)}">
 								<img
 									src={prof?.picture ?? getRoboHashURL(webbookmark.pubkey)}
 									alt=""
 									class="avatar"
-								/>@{prof?.name ?? ''}
+								/>
 							</a>
-							{#each hashtags as hashtag (hashtag)}
-								<a href="/t/{encodeURI(hashtag)}" class="hashtag">#{hashtag}</a>
-							{/each}
-						</dt>
-						<dd>
-							{webbookmark.content}
+						</div>
+						<div class="contents">
+							<div class="comment">
+								<a class="name" href="/{nip19.npubEncode(webbookmark.pubkey)}"
+									>@{prof?.name ?? ''}</a
+								>
+								<span class="content">{webbookmark.content}</span>
+								{#each hashtags as hashtag (hashtag)}
+									<a href="/t/{encodeURI(hashtag)}" class="hashtag">#{hashtag}</a>
+								{/each}
+							</div>
+							<div class="menu">
+								{#if loginPubkey !== undefined}
+									<button
+										type="button"
+										class="fork"
+										onclick={() => {
+											editDTag = identifier;
+											editTitleTag = title;
+											editTag = '';
+											editTags = Array.from(hashtags);
+											editContent = webbookmark.content;
+											isOpenEdit = true;
+											setTimeout(() => {
+												editTagInput?.focus();
+											}, 10);
+										}}>Fork</button
+									>
+								{/if}
+								<a href="/{naddr}">
+									<time class="created_at"
+										>{new Date(1000 * webbookmark.created_at).toLocaleString()}</time
+									>
+								</a>
+								{#if loginPubkey === webbookmark.pubkey}
+									<span class="bookmark-delete">
+										<button
+											class="bookmark-delete"
+											title="delete the bookmark"
+											onclick={async () => {
+												await sendDeletion(webbookmark);
+											}}
+											aria-label="delete the bookmark"
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												width="24"
+												height="24"
+												viewBox="0 0 24 24"
+											>
+												<path
+													fill-rule="evenodd"
+													d="M7,4 L7,3 C7,1.8954305 7.8954305,1 9,1 L15,1 C16.1045695,1 17,1.8954305 17,3 L17,4 L20,4 C21.1045695,4 22,4.8954305 22,6 L22,8 C22,9.1045695 21.1045695,10 20,10 L19.9198662,10 L19,21 C19,22.1045695 18.1045695,23 17,23 L7,23 C5.8954305,23 5,22.1045695 5.00345424,21.0830455 L4.07986712,10 L4,10 C2.8954305,10 2,9.1045695 2,8 L2,6 C2,4.8954305 2.8954305,4 4,4 L7,4 Z M7,6 L4,6 L4,8 L20,8 L20,6 L17,6 L7,6 Z M6.08648886,10 L7,21 L17,21 L17.0034542,20.9169545 L17.9132005,10 L6.08648886,10 Z M15,4 L15,3 L9,3 L9,4 L15,4 Z"
+												/>
+											</svg>
+										</button>
+									</span>
+								{/if}
+								<AddStar
+									sendReaction={getSendReaction(webbookmark)}
+									{sendDeletion}
+									{loginPubkey}
+									{profileMap}
+									eventsReactionToTheTarget={getEventsReactionToTheTarget(
+										webbookmark,
+										eventsReaction
+									)}
+									{eventsEmojiSet}
+									mutedWords={[]}
+								/>
+							</div>
 							{#if isDevMode}
 								<details class="details">
 									<summary>view JSON</summary>
@@ -431,9 +439,9 @@
 									</dl>
 								</details>
 							{/if}
-						</dd>
-					{/each}
-				</dl>
+						</div>
+					</div>
+				{/each}
 			</dd>
 		{/each}
 	</dl>
@@ -460,17 +468,6 @@
 	summary {
 		width: 100%;
 	}
-	.url > dt {
-		border-radius: 5px;
-		padding: 5px;
-		background-color: rgba(0, 0, 0, 0.2);
-	}
-	.entry > dt {
-		margin-top: 1em;
-	}
-	.entry > dd {
-		margin-bottom: 1em;
-	}
 	dl.edit input,
 	dl.edit textarea {
 		min-width: 15em;
@@ -481,39 +478,64 @@
 	.d-tag > input {
 		display: inline-block;
 	}
-	.fork {
-		padding: 0 3px;
-	}
 	input#edit-category:invalid,
 	input.invalid {
 		outline: 2px solid red;
+	}
+	.url > dt {
+		border-radius: 5px;
+		padding: 5px;
+		background-color: rgba(0, 0, 0, 0.2);
+	}
+	.favicon {
+		width: 16px;
+		height: 16px;
+		border-radius: 10%;
+	}
+	span.url {
+		font-size: small;
 	}
 	.bookmark-count {
 		display: inline-block;
 		color: pink;
 		text-shadow: 0 0 2px white;
 	}
-	span.url {
-		font-size: small;
+	.entry {
+		display: flex;
+		margin-top: 1em;
+		margin-bottom: 1em;
 	}
-	.entry > dt {
-		position: relative;
+	.contents {
+		width: calc(100% - 64px);
 	}
-	.entry > dd {
+	.avatar {
+		width: 48px;
+		height: 48px;
+	}
+	div.avatar {
+		margin-right: 0.5em;
+	}
+	img.avatar {
+		border-radius: 10%;
+	}
+	.comment {
 		white-space: pre-line;
 	}
-	.details {
-		overflow-x: auto;
+	.menu {
+		margin-top: 2px;
 	}
-	.favicon,
-	.avatar {
-		width: 16px;
-		height: 16px;
-		border-radius: 10%;
+	.fork {
+		padding: 0 3px;
 	}
 	.category-tag,
 	.hashtag {
 		margin-left: 0.5em;
+	}
+	.created_at {
+		font-size: small;
+	}
+	.details {
+		overflow-x: auto;
 	}
 	button.category-delete,
 	button.bookmark-delete {
