13bb96182ed68e7f37ee02ade6480c82df97cc12
Don <496156+nikolat@users.noreply.github.com>
2025-05-25 16:12:07 +0900

    refactoring

--- src/lib/components/Content.svelte
+++ src/lib/components/Content.svelte
@@ -41,7 +41,48 @@
 		getReplaceableEvent: (kind: number, pubkey: string, d?: string) => NostrEvent | undefined;
 	} = $props();
 
-	const getExpandTagsList = (content: string): [IterableIterator<RegExpMatchArray>, string[]] => {
+	type Token =
+		| {
+				type: 'text';
+				value: string;
+		  }
+		| {
+				type: 'link';
+				href: string;
+				value: string;
+		  }
+		| {
+				type: 'mention';
+				decoded:
+					| {
+							type: 'nevent';
+							data: nip19.EventPointer;
+					  }
+					| {
+							type: 'nprofile';
+							data: nip19.ProfilePointer;
+					  }
+					| {
+							type: 'naddr';
+							data: nip19.AddressPointer;
+					  }
+					| {
+							type: 'npub';
+							data: string;
+					  }
+					| {
+							type: 'nsec';
+							data: Uint8Array;
+					  }
+					| {
+							type: 'note';
+							data: string;
+					  };
+				encoded: string;
+				value: string;
+		  };
+
+	const getExpandTagsList = (content: string) => {
 		const regMatchArray = [
 			'https?://[\\w!?/=+\\-_~:;.,*&@#$%()[\\]]+',
 			'nostr:npub1\\w{58}',
@@ -52,10 +93,47 @@
 		];
 		const regMatch = new RegExp(regMatchArray.map((v) => `(${v})`).join('|'), 'g');
 		const regSplit = new RegExp(regMatchArray.join('|'));
-		return [content.matchAll(regMatch), content.split(regSplit)];
-	};
 
-	const [matchesIterator, plainTexts] = $derived(getExpandTagsList(content));
+		const plainTexts = content.split(regSplit);
+		const matchesIterator = content.matchAll(regMatch);
+		const children: [Token] = [
+			{
+				type: 'text',
+				value: plainTexts[0]
+			}
+		];
+		let i = 1;
+		for (const m of matchesIterator) {
+			const mLink = m.at(1);
+			const mMention = m.at(2) ?? m.at(3) ?? m.at(4) ?? m.at(5) ?? m.at(6);
+			const mMentionDecoded =
+				mMention === undefined ? null : nip19decode(mMention.replace(/nostr:/, ''));
+			if (mLink !== undefined && /^https?:\/\/\S+/.test(mLink) && URL.canParse(mLink)) {
+				children.push({
+					type: 'link',
+					href: new URL(mLink).toString(),
+					value: mLink
+				});
+			} else if (mMention !== undefined && mMentionDecoded !== null) {
+				children.push({
+					type: 'mention',
+					decoded: mMentionDecoded,
+					encoded: mMention.replace(/nostr:/, ''),
+					value: mMention
+				});
+			}
+			children.push({
+				type: 'text',
+				value: plainTexts[i]
+			});
+			i++;
+		}
+		return {
+			type: 'root',
+			event: undefined,
+			children
+		};
+	};
 
 	const nip19decode = (text: string) => {
 		try {
@@ -64,58 +142,37 @@
 			return null;
 		}
 	};
+
+	const ats = $derived(getExpandTagsList(content));
 </script>
 
-{plainTexts[0]}{#each Array.from(matchesIterator) as match, i (i)}
-	{@const urlHttp = match[1]}
-	{@const nostr_npub1 = match[2]}
-	{@const nostr_nprofile1 = match[3]}
-	{@const nostr_note1 = match[4]}
-	{@const nostr_nevent1 = match[5]}
-	{@const nostr_naddr1 = match[6]}
-	{#if /^https?:\/\/\S+/.test(urlHttp) && URL.canParse(urlHttp)}
-		{@const [url, rest] = urlLinkString(urlHttp)}
+{#each ats.children as ct, i (i)}
+	{#if ct.type === 'link'}
+		{@const [url, rest] = urlLinkString(ct.value)}
 		<a href={url} target="_blank" rel="noopener noreferrer">{url}</a>{rest}
-	{:else if /nostr:npub1\w{58}/.test(nostr_npub1)}
-		{@const matchedText = nostr_npub1}
-		{@const npubText = matchedText.replace(/nostr:/, '')}
-		{@const d = nip19decode(npubText)}
-		{#if d?.type === 'npub'}
-			{@const prof = profileMap.get(d.data)}
-			{@const name = prof?.name ?? nip19.npubEncode(d.data)}
-			<a href="/{npubText}"
+	{:else if ct.type === 'mention'}
+		{@const d = ct.decoded}
+		{#if ['npub', 'nprofile'].includes(d.type)}
+			{@const hex = d.type === 'npub' ? d.data : d.type === 'nprofile' ? d.data.pubkey : ''}
+			{@const enc = ct.encoded}
+			{@const prof = profileMap.get(hex)}
+			{@const name = prof?.name ?? enc}
+			<a href="/{enc}"
 				><img
-					src={prof?.picture ?? getRoboHashURL(d.data)}
+					src={prof?.picture ?? getRoboHashURL(hex)}
 					alt={name.slice(0, 20)}
 					title={name.slice(0, 20)}
 					class="avatar"
 				/>{name.slice(0, 20)}</a
 			>
-		{:else}{matchedText}
-		{/if}
-	{:else if /nostr:nprofile1\w+/.test(nostr_nprofile1)}
-		{@const matchedText = nostr_nprofile1}
-		{@const nprofileText = matchedText.replace(/nostr:/, '')}
-		{@const d = nip19decode(nprofileText)}
-		{#if d?.type === 'nprofile'}
-			{@const prof = profileMap.get(d.data.pubkey)}
-			{@const name = prof?.name ?? nip19.npubEncode(d.data.pubkey)}
-			<a href="/{nprofileText}"
-				><img
-					src={prof?.picture ?? getRoboHashURL(d.data.pubkey)}
-					alt={name.slice(0, 20)}
-					title={name.slice(0, 20)}
-					class="avatar"
-				/>{name.slice(0, 20)}</a
-			>
-		{:else}{matchedText}
-		{/if}
-	{:else if /nostr:note1\w{58}/.test(nostr_note1)}
-		{@const matchedText = nostr_note1}
-		{@const note = matchedText.replace(/nostr:/, '')}
-		{@const d = nip19decode(note)}
-		{#if d?.type === 'note'}
-			{@const event = getEventsByFilter({ ids: [d.data] }).at(0)}
+		{:else if ['note', 'nevent', 'naddr'].includes(d.type)}
+			{@const event =
+				d.type === 'naddr'
+					? getReplaceableEvent(d.data.kind, d.data.pubkey, d.data.identifier)
+					: getEventsByFilter({
+							ids: [d.type === 'note' ? d.data : d.type === 'nevent' ? d.data.id : '']
+						}).at(0)}
+			{@const enc = ct.encoded}
 			{#if event !== undefined}
 				<Entry
 					{event}
@@ -135,69 +192,12 @@
 					{getReplaceableEvent}
 				/>
 			{:else}
-				<a href={`/${note}`}>{matchedText}</a>
+				<a href={`/${enc}`}>{`nostr:${enc}`}</a>
 			{/if}
-		{:else}{matchedText}
 		{/if}
-	{:else if /nostr:nevent1\w+/.test(nostr_nevent1)}
-		{@const matchedText = nostr_nevent1}
-		{@const nevent = matchedText.replace(/nostr:/, '')}
-		{@const d = nip19decode(nevent)}
-		{#if d?.type === 'nevent'}
-			{@const event = getEventsByFilter({ ids: [d.data.id] }).at(0)}
-			{#if event !== undefined}
-				<Entry
-					{event}
-					{eventsComment}
-					level={level + 1}
-					{idReferenced}
-					{getSeenOn}
-					{fork}
-					{sendComment}
-					{sendReaction}
-					{sendDeletion}
-					{loginPubkey}
-					{profileMap}
-					{eventsReaction}
-					{eventsEmojiSet}
-					{getEventsByFilter}
-					{getReplaceableEvent}
-				/>
-			{:else}
-				<a href={`/${nevent}`}>{matchedText}</a>
-			{/if}
-		{:else}{matchedText}
-		{/if}
-	{:else if /nostr:naddr1\w+/.test(nostr_naddr1)}
-		{@const matchedText = nostr_naddr1}
-		{@const naddr = matchedText.replace(/nostr:/, '')}
-		{@const d = nip19decode(naddr)}
-		{#if d?.type === 'naddr'}
-			{@const event = getReplaceableEvent(d.data.kind, d.data.pubkey, d.data.identifier)}
-			{#if event !== undefined}
-				<Entry
-					{event}
-					{eventsComment}
-					level={level + 1}
-					{idReferenced}
-					{getSeenOn}
-					{fork}
-					{sendComment}
-					{sendReaction}
-					{sendDeletion}
-					{loginPubkey}
-					{profileMap}
-					{eventsReaction}
-					{eventsEmojiSet}
-					{getEventsByFilter}
-					{getReplaceableEvent}
-				/>
-			{:else}
-				<a href={`/${naddr}`}>{matchedText}</a>
-			{/if}
-		{:else}{matchedText}
-		{/if}
-	{/if}{plainTexts[i + 1]}
+	{:else if ct.type === 'text'}
+		{ct.value}
+	{/if}
 {/each}
 
 <style>
